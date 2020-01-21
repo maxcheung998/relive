ce38f61f27f35724c2409fc422a16863151789f1
qlmaxcheung <max.c@quan-li.com>
2020-01-21 22:09:23 +0800

    Change Loading Image, and create questionnaire files

---
+++ chatapp/src/main/java/jiguang/chat/activity/QuestionnaireActivity.java
@@ -0,0 +1,15 @@
+package jiguang.chat.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jiguang.chat.R;
+
+public class QuestionnaireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionnaire);
+    }
+}
