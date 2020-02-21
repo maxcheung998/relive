c7776429da418960792c24b7e05ef701c70ff6f8
qldeveloper <qldeveloper@163.com>
2020-02-21 18:18:40 +0800

    update chatbot web into app

--- chatapp/src/main/java/jiguang/chat/activity/ChatbotActivity.java
+++ chatapp/src/main/java/jiguang/chat/activity/ChatbotActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -33,7 +35,12 @@ public class ChatbotActivity extends BaseActivity implements View.OnClickListene
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatbot);
         initView();
-        initListener();
+        //initListener();
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://bot.dialogflow.com/3ae3fbff-c653-447b-be85-8ccc27153362");
+
     }
 
     private void initView() {
@@ -41,14 +48,14 @@ public class ChatbotActivity extends BaseActivity implements View.OnClickListene
         mBtn_start = (Button) findViewById(R.id.btn_sure);
     }
     private void initListener() {
-        mBtn_start.setOnClickListener(this);
+//        mBtn_start.setOnClickListener(this);
     }
-
+//
     @Override
     public void onClick(View v) {
-        Uri uri = Uri.parse("https://bot.dialogflow.com/b2a897fa-3eb6-48ec-bcf1-dfab0e830c94");
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(intent);
-
+//        Uri uri = Uri.parse("https://bot.dialogflow.com/b2a897fa-3eb6-48ec-bcf1-dfab0e830c94");
+//        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//        startActivity(intent);
+//
     }
 }
