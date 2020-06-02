d3cee83973f45bffd3dd652e3347efcda4cc15f8
qldeveloper <qldeveloper@163.com>
2020-06-02 16:12:55 +0800

    change color, update img

--- chatapp/src/main/java/jiguang/chat/activity/VerificationMessageActivity.java
+++ chatapp/src/main/java/jiguang/chat/activity/VerificationMessageActivity.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 import jiguang.chat.R;
 import jiguang.chat.activity.fragment.FriendFragment;
-import jiguang.chat.activity.fragment.GroupFragment;
 import jiguang.chat.adapter.ViewPagerAdapter;
 import jiguang.chat.view.NoScrollViewPager;
 
@@ -39,7 +38,6 @@ public class VerificationMessageActivity extends FragmentActivity {
 
         mFragmentList = new ArrayList<>();
         mFragmentList.add(new FriendFragment());
-        mFragmentList.add(new GroupFragment());
         mRg.check(R.id.rb_friend);
 
         mPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager(), mFragmentList));
@@ -48,9 +46,6 @@ public class VerificationMessageActivity extends FragmentActivity {
             switch (checkedId) {
                 case R.id.rb_friend:
                     mCurTabIndex = 0;
-                    break;
-                case R.id.rb_group:
-                    mCurTabIndex = 1;
                     break;
                 default:
                     break;
